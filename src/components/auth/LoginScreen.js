a37fdaef2d9891d778bc3aa65fff29a47f2590e0
Ignacio <dev.ignacio.h@gmail.com>
2022-03-07 17:56:03 -0300

    connect auth API and routes

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,25 +1,65 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import Swal from 'sweetalert2';
+import { startLogin, startRegister } from '../../actions/auth';
+import useForm from '../../hooks/useForm';
 import './login.css';
 
 const LoginScreen = () => {
+  const dispatch = useDispatch();
+
+  const [formLoginValues, handleLoginInputChange] = useForm({
+    lEmail: 'ignacio@mail.com',
+    lPassword: '123456',
+  });
+
+  const [formRegisterValues, handleRegisterInputChange] = useForm({
+    rEmail: 'catalina@mail.com',
+    rName: 'Catalina',
+    rPassword1: '123456',
+    rPassword2: '123456',
+  });
+
+  const { lEmail, lPassword } = formLoginValues;
+  const { rName, rEmail, rPassword1, rPassword2 } = formRegisterValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(startLogin(lEmail, lPassword));
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    if (rPassword1 !== rPassword2) {
+      return Swal.fire('Error', 'Paswords should be the same', 'error');
+    }
+    dispatch(startRegister(rEmail, rPassword1, rName));
+  };
+
   return (
     <div className="container login-container">
       <div className="row">
         <div className="col-md-6 login-form-1">
           <h3>Ingreso</h3>
-          <form>
+          <form onSubmit={handleLogin}>
             <div className="form-group">
               <input
                 type="text"
                 className="form-control"
-                placeholder="Correo"
+                placeholder="Email"
+                name="lEmail"
+                value={lEmail}
+                onChange={handleLoginInputChange}
               />
             </div>
             <div className="form-group">
               <input
                 type="password"
                 className="form-control"
-                placeholder="Contraseña"
+                placeholder="Password"
+                name="lPassword"
+                value={lPassword}
+                onChange={handleLoginInputChange}
               />
             </div>
             <div className="form-group">
@@ -30,26 +70,35 @@ const LoginScreen = () => {
 
         <div className="col-md-6 login-form-2">
           <h3>Registro</h3>
-          <form>
+          <form onSubmit={handleRegister}>
             <div className="form-group">
               <input
                 type="text"
                 className="form-control"
-                placeholder="Nombre"
+                placeholder="Name"
+                name="rName"
+                value={rName}
+                onChange={handleRegisterInputChange}
               />
             </div>
             <div className="form-group">
               <input
                 type="email"
                 className="form-control"
-                placeholder="Correo"
+                placeholder="Email"
+                name="rEmail"
+                value={rEmail}
+                onChange={handleRegisterInputChange}
               />
             </div>
             <div className="form-group">
               <input
                 type="password"
                 className="form-control"
-                placeholder="Contraseña"
+                placeholder="Password"
+                name="rPassword1"
+                value={rPassword1}
+                onChange={handleRegisterInputChange}
               />
             </div>
 
@@ -57,7 +106,10 @@ const LoginScreen = () => {
               <input
                 type="password"
                 className="form-control"
-                placeholder="Repita la contraseña"
+                placeholder="Repite el password"
+                name="rPassword2"
+                value={rPassword2}
+                onChange={handleRegisterInputChange}
               />
             </div>
 
--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,11 @@ export const types = {
   eventClearActiveEvent: '[event] Clear active event',
   eventUpdated: '[event] Event updated',
   eventDeleted: '[event] Event deleted',
+
+  authChecking: '[auth] Checking login state',
+  authCheckingFinish: '[auth] Finish checking login state',
+  authStartLogin: '[auth] Start login',
+  authLogin: '[auth] Login',
+  authStartTokenRenew: '[auth] Start token renew',
+  authLogout: '[auth] Logout',
 };
